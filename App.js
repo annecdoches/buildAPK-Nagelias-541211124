108a1bb12d7f4ea4cdff9365d2c7b95d0fbd61a0
nagelia <nageliasalbila@gmail.com>
2024-01-10 15:30:07 +0700

    build APK

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Text>im Nagel, a student from SMK Telkom Purwokerto</Text>
       <StatusBar style="auto" />
     </View>
   );
